cad89608d8285000d8f6d39e9f9c9ac8f2b6c43b
stepholdcorn <stephieoldcorn@gmail.com>
2015-01-19 09:24:50 +0000

    Adds styling to error messages

--- lib/user.rb
+++ lib/user.rb
@@ -23,11 +23,7 @@ class User
 
 	def self.authenticate(email, password)
 		user = first(email: email)
-		if user && BCrypt::Password.new(user.password_digest) == password
-			user
-		else
-			nil
-		end
+		(user && BCrypt::Password.new(user.password_digest) == password) ? user : nil
 	end
 
 end
